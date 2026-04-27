546a2fb8945341bc218534bb00989c4925ba7725
IagorSps <iagor.sps+github@gmail.com>
2026-04-27 16:40:55 -0300

    feat(swagger): :memo: Setup swagger integrated with API

--- applications/backend/src/main.ts
+++ applications/backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Smart To-Do List')
+    .setDescription('List of tasks integrated with AI agent to be powerful!')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
